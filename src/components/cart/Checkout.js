609ff5dad4a6bd881b4170bff0859fab03af0eb7
BuildTools <unconfigured@null.spigotmc.org>
2021-06-08 08:35:03 -0400

    Order form with formik

--- src/components/cart/Checkout.js
+++ src/components/cart/Checkout.js
@@ -1,33 +1,63 @@
 import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import Card from '../UI/Card';
 
 import './Checkout.css';
 
 const Checkout = props => {
+
     const formSubmitHandler = event => {
         event.preventDefault();
-
         props.toggleCart();
     }
 
     return (
         <Card id="checkout">
             <h2>Enter your contact information</h2>
-            <form className="checkout-form" onSubmit={formSubmitHandler}>
-                <div className="inputs">
-                    <div>
-                        <label htmlFor="name">Full name</label>
-                        <input type="text" id="name" />
+            <Formik
+                initialValues={{ fullName: '', streetAddress: '', city: '', state: '', zipCode: '' }}
+                validationSchema={Yup.object({
+                    fullName: Yup.string().required('Please enter your full name'),
+                    streetAddress: Yup.string().required('Please enter your street address'),
+                    city: Yup.string().required('Please enter your city'),
+                    state: Yup.string().required('Please enter your state'),
+                    zipCode: Yup.number().min(10000, 'Please enter your zip code').max(99999, 'Please enter your zip code').required('Please enter your zip code')
+                })}
+                onSubmit={formSubmitHandler}
+            >
+                <Form>
+                    <div className="form-line">
+                        <label htmlFor="fullName">Full Name</label>
+                        <Field name="fullName" type="text" />
+                        <div className="error-text"><ErrorMessage name="fullName" /></div>
                     </div>
-                    <div>
-                        <label htmlFor="email">Email address</label>
-                        <input type="text" id="email" />
+                    <div className="form-line">
+                        <label htmlFor="streetAddress">Street Address</label>
+                        <Field name="streetAddress" type="text" />
+                        <div className="error-text"><ErrorMessage name="streetAddress" /></div>
                     </div>
-                </div>
-                <div className="actions">
-                    <button className="button" type="submit">Submit</button>   
-                </div>
-            </form>
+                    <div className="form-line">
+                        <label htmlFor="city">City</label>
+                        <Field name="city" type="text" />
+                        <div className="error-text"><ErrorMessage name="city" /></div>
+                    </div>
+                    <div className="form-line">
+                        <label htmlFor="state">State</label>
+                        <Field name="state" type="text" minlength="2" maxlength="2" size="3" />
+                        <div className="error-text"><ErrorMessage name="state" /></div>
+                    </div>
+                    <div className="form-line">
+                        <label htmlFor="zipCode">Zip Code</label>
+                        <Field name="zipCode" type="text" minlength="5" maxlength="5" size="5" />
+                        <div className="error-text"><ErrorMessage name="zipCode" /></div>
+                    </div>
+                    <div className="actions">
+                        <button className="button--alt" onClick={props.toggleCart}>Cancel</button>
+                        <button className="button" type="submit">Submit</button>
+                    </div>
+                </Form>
+            </Formik>
         </Card>
     );
 };
