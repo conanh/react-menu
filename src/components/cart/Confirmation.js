878c295d1f29bc0cb5362efe292a3b7eef1a1b34
BuildTools <unconfigured@null.spigotmc.org>
2021-06-11 09:12:57 -0400

    Style confirmation dialog a little nicer

--- src/components/cart/Confirmation.js
+++ src/components/cart/Confirmation.js
@@ -11,17 +11,19 @@ const Confirmation = () => {
     <Card id="confirmation">
       <h2>Thanks for your order!</h2>
       <p>We'll have it delivered soon.</p>
-      <div className="orded-items">
-        {cart.map(item => {
-          return (
-            <div key={item.id}>{item.quantity} x {item.name}</div>
-          );
-        })}
-      </div>
-      <div className="contact-info">
-        <div>{contact.fullName}</div>
-        <div>{contact.streetAddress}</div>
-        <div>{contact.city}, {contact.state} &nbsp; {contact.zipCode}</div>
+      <div className="confirmation-info">
+        <div className="orded-items">
+          {cart.map(item => {
+            return (
+              <div key={item.id}>{item.quantity} x {item.name}</div>
+            );
+          })}
+        </div>
+        <div className="contact-info">
+          <div>{contact.fullName}</div>
+          <div>{contact.streetAddress}</div>
+          <div>{contact.city}, {contact.state} &nbsp; {contact.zipCode}</div>
+        </div>
       </div>
     </Card>
   );
